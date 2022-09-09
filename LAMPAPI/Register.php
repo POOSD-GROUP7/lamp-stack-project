89d725c939f30d05c09b37db9ef7d87e5e063d6c
Aaronel10 <aaronelharo@hotmail.com>
2022-09-09 17:11:53 +0000

    returns User credentials after creation, returns 409 if username is taken

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -10,6 +10,10 @@
 		exit;
 	}
 
+	// ini_set('display_errors', 1);
+    // ini_set('display_startup_errors', 1);
+    // error_reporting(E_ALL);
+
 	$inData = getRequestInfo();
 
 	$firstName = $inData["firstName"];
@@ -24,13 +28,35 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
-		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+		$sql = "SELECT * FROM Users WHERE Login=?";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("s", $login);
 		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		http_response_code(201);
-		returnWithInfo("Successful Creation");
+		$result = $stmt->get_result();
+		$rows = mysqli_num_rows($result);
+		if ($rows == 0) 
+		{
+			$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+			$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+			$stmt->execute();
+			$stmt->close();
+			$conn->close();
+			http_response_code(201);
+			$searchResults .= '{'.
+			
+				'"firstName": "'.$firstName.'", '.
+
+				'"lastName": "'.$lastName.'", '.
+
+				'"Login": "'.$login.'", '.
+
+				'"Password": "'.$password.''.'"}';
+
+			returnWithInfo($searchResults);
+		} else {
+			http_response_code(409);
+			returnWithError("Username taken");
+		}
 	}
 
 	function getRequestInfo()
